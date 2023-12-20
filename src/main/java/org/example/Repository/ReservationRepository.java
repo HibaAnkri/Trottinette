2a56ebe2aa6e4a46778b66146b50e5b265db1825
hiba <hibaankri9@gmail.com>
2023-12-20 16:01:22 +0100

    mmodifications

--- src/main/java/org/example/Repository/ReservationRepository.java
+++ src/main/java/org/example/Repository/ReservationRepository.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @Repository
 public interface ReservationRepository extends JpaRepository<Reservation,Long> {
-     List<Reservation> findByTrottinetteReser(Trottinette trottinette, Date dateDebut, Date dateFin) ;
+
 }
 
 
